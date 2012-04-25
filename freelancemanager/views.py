ba506fb262f2e1fa9fabba19b08d2312e0578579
Jose <jose.ruzafa@gmail.com>
2012-04-25 02:36:06 +0200

    index works OK

--- freelancemanager/views.py
+++ freelancemanager/views.py
@@ -1,9 +1,17 @@
 # Create your views here.
+from django.template import Context, loader
 from django.http import HttpResponse
-
+from freelancemanager.models import Work
 
 def index(request):
-	return HttpResponse("Hello, world. You're at the work view")
+	all_works = Work.objects.all()
+	t = loader.get_template('works/index.html')
+	c = Context({
+		'all_works': all_works,
+	})
+
+	#return HttpResponse("Hello, world. You're at the work view")
+	return HttpResponse(t.render(c))
 
 def detail(request, work_id):
 	return HttpResponse("You're looking at the results of work %s." % work_id)
