e03f943ba286ae5b52ddbe047838b8a60ae0d977
Jose <jose.ruzafa@gmail.com>
2012-04-27 14:29:38 +0200

    enjoy views

--- freelancemanager/views.py
+++ freelancemanager/views.py
@@ -1,20 +1,33 @@
 # Create your views here.
 from django.template import Context, loader
-from django.http import HttpResponse
+from django.http import HttpResponse,Http404
 from freelancemanager.models import Work
 
-def index(request):
-	all_works = Work.objects.all()
-	t = loader.get_template('works/index.html')
-	c = Context({
-		'all_works': all_works,
-	})
+from django.shortcuts import render_to_response, get_object_or_404
 
-	#return HttpResponse("Hello, world. You're at the work view")
-	return HttpResponse(t.render(c))
+def index(request):
+	# all_works = Work.objects.all()
+	# t = loader.get_template('works/index.html')
+	# c = Context({
+	# 	'all_works': all_works,
+	# })
+
+	# #return HttpResponse("Hello, world. You're at the work view")
+	# return HttpResponse(t.render(c))
+	all_works = Work.objects.all()
+	return render_to_response('works/index.html', {'all_works': all_works})
 
 def detail(request, work_id):
-	return HttpResponse("You're looking at the results of work %s." % work_id)
+	# return HttpResponse("You're looking at the results of work %s." % work_id)
+
+	# try:
+	# 	w = Work.objects.get(pk=work_id)
+	# except Work.DoesNotExist:
+	# 	raise Http404
+	# return render_to_response('works/detail.html',{'work': w})	
+
+	w = get_object_or_404(Work, pk=work_id)
+	return render_to_response('works/detail.html', {'work': w})
 
 def customer(request, customer_id):
 	return HttpResponse("You're looking at the results of customer %s." % customer_id)
